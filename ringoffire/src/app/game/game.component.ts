01a110fb8d299efb147e665fdae8b296c01f90ec
Jan Steiner <jansteiner73@gmail.com>
2025-01-15 09:21:37 +0100

    player created

--- ringoffire/src/app/game/game.component.ts
+++ ringoffire/src/app/game/game.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Game } from './../../models/game';
+import { PlayerComponent } from "../player/player.component";
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [ CommonModule ],
+  imports: [CommonModule, PlayerComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
@@ -12,10 +14,30 @@ export class GameComponent {
 
   pickCardAnimation = false;
 
-  constructor() {}
+  game!: Game;
+  currentCard: string = '';
+  playedCard: string = '';
+
+  constructor() {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+  }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop() || '';
+      console.log(this.game);
+      this.pickCardAnimation = true;
+
+      setTimeout(() => {
+        this.playedCard = this.currentCard;
+        this.pickCardAnimation = false;
+        this.game.playedCards.push(this.playedCard);
+      }, 1000);
+    }
   }
 
 }
---
+++ ringoffire/src/models/game.ts
@@ -0,0 +1,21 @@
+export class Game {
+    public players: string[] = [];
+    public stack: string[] = [];
+    public playedCards: string[] = [];
+    public currentPlayer: number = 0;
+
+    constructor() {
+        for (let i = 1; i < 14; i++) {
+            this.stack.push(`ace_${i}`)
+            this.stack.push(`hearts_${i}`)
+            this.stack.push(`clubs_${i}`)
+            this.stack.push(`diamonds_${i}`)
+        };
+
+        const shuffle = (arr: string[]) => {
+            return arr.sort(() => 0.5 - Math.random());
+        };
+
+        this.stack = shuffle(this.stack);
+    }
+}
